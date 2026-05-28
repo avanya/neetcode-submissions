e885b8d4818a28ab35205481fec883398076f200
avanya <40421402+avanya@users.noreply.github.com>
2026-05-28 13:35:09 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+
+        var i = 0
+        var j = s.count - 1
+
+        while i <= j{
+            var a = s[i]
+            s[i] = s[j]
+            s[j] = a
+
+            i += 1
+            j -= 1
+        }
+
+    }
+}
