8dcd39f117e089a8261a81c4ec5fb9fa9a764dd8
avanya <40421402+avanya@users.noreply.github.com>
2026-06-03 19:07:04 +0530

    Add: factory - submission-0

---
+++ Design Patterns/factory/submission-0.swift
@@ -0,0 +1,51 @@
+protocol Vehicle {
+    func getType() -> String
+}
+
+class Car: Vehicle {
+    func getType() -> String {
+        return "Car"
+    }
+}
+
+class Bike: Vehicle {
+    func getType() -> String {
+        return "Bike"
+    }
+}
+
+class Truck: Vehicle {
+    func getType() -> String {
+        return "Truck"
+    }
+}
+
+class VehicleFactory {
+    func createVehicle() -> Vehicle {
+        fatalError("Subclasses must override")
+    }
+}
+
+class CarFactory: VehicleFactory {
+    // Write your code here
+    override func createVehicle() -> Vehicle {
+        var c = Car()
+        return c
+    }
+}
+
+class BikeFactory: VehicleFactory {
+    // Write your code here
+    override func createVehicle() -> Vehicle {
+        var c = Bike()
+        return c
+    }
+}
+
+class TruckFactory: VehicleFactory {
+    // Write your code here
+    override func createVehicle() -> Vehicle {
+        var c = Truck()
+        return c
+    }
+}
